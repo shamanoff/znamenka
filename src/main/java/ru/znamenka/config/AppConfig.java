0da58b5bdef7ba46916db61b9631599e17c6f984
Evgeny.Utkin <evgeny.utkin@tns-global.ru>
2016-08-04 19:06:20 +0300

    перенес бины в отдельный конфиг

--- src/main/java/ru/znamenka/config/AppConfig.java
+++ src/main/java/ru/znamenka/config/AppConfig.java
@@ -1,15 +1,9 @@
 package ru.znamenka.config;
 
-import lombok.val;
-import org.springframework.aop.framework.ProxyFactoryBean;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.gson.GsonAutoConfiguration;
-import org.springframework.context.MessageSource;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import ru.znamenka.util.locale.ExtResourceBundleSource;
 
 /**
  * <p>
@@ -23,35 +17,8 @@ import ru.znamenka.util.locale.ExtResourceBundleSource;
         , BeanBucketConfig.class
         , WebConfig.class
         , CacheConfig.class
+        , BeanConfig.class
 })
-@ComponentScan("ru.znamenka.*")
 @EnableAutoConfiguration
 public class AppConfig {
-
-
-    /**
-     * Proxy для перехвата исключений
-     * @return возвращает proxy
-     */
-    @Bean(name = "dataService")
-    public ProxyFactoryBean facadeRepoProxy() {
-        ProxyFactoryBean proxy = new ProxyFactoryBean();
-        proxy.setInterceptorNames("repositoryThrowsAdvice");
-        proxy.setTargetName("convertService");
-
-        return proxy;
-    }
-
-    /**
-     * Создает бин для доступа к пулу сообщений.
-     * Используется для локализации
-     * @return бин для доступа к локализованным сообщениям
-     */
-    @Bean
-    public MessageSource messageSource() {
-        val source = new ExtResourceBundleSource();
-        source.setBasename("static\\locale\\messages");
-        source.setDefaultEncoding("UTF-8");
-        return source;
-    }
 }
---
+++ src/main/java/ru/znamenka/config/BeanConfig.java
@@ -0,0 +1,66 @@
+package ru.znamenka.config;
+
+import lombok.val;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
+import org.apache.commons.configuration.reloading.FileChangedReloadingStrategy;
+import org.springframework.aop.framework.ProxyFactoryBean;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.*;
+import ru.znamenka.util.locale.ExtResourceBundleSource;
+
+/**
+ * <p>Конфигуратор бинов
+ * <p>
+ * Создан 04.08.2016
+ * <p>
+ * Изменения:
+ * <p>
+ *
+ * @author Евгений Уткин (Eugene Utkin)
+ */
+@Configuration
+@ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class))
+public class BeanConfig {
+
+    /**
+     * Proxy для перехвата исключений
+     * @return возвращает proxy
+     */
+    @Bean(name = "dataService")
+    public ProxyFactoryBean facadeRepoProxy() {
+        ProxyFactoryBean proxy = new ProxyFactoryBean();
+        proxy.setInterceptorNames("repositoryThrowsAdvice");
+        proxy.setTargetName("convertService");
+
+        return proxy;
+    }
+
+    /**
+     * Создает бин для доступа к пулу сообщений.
+     * Используется для локализации
+     * @return бин для доступа к локализованным сообщениям
+     */
+    @Bean
+    public MessageSource messageSource() {
+        val source = new ExtResourceBundleSource();
+        source.setBasename("static\\locale\\messages");
+        source.setDefaultEncoding("UTF-8");
+        return source;
+    }
+
+    @Bean(initMethod = "load")
+    public PropertiesConfiguration configuration() throws ConfigurationException {
+        val config = new PropertiesConfiguration("constants.properties");
+        config.setReloadingStrategy(fileChangedStrategy());
+        config.setListDelimiter(',');
+
+        return config;
+    }
+
+    @Bean
+    public FileChangedReloadingStrategy fileChangedStrategy() {
+        return new FileChangedReloadingStrategy();
+    }
+
+}
