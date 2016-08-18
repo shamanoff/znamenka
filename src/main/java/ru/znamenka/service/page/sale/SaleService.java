0a269e12ee848a0fb0dff93a45455f83fb6221d2
eutkin <utkinev2014@gmail.com>
2016-08-18 19:11:45 +0300

    -

--- src/main/java/ru/znamenka/service/page/sale/SaleService.java
+++ src/main/java/ru/znamenka/service/page/sale/SaleService.java
@@ -4,12 +4,10 @@ import com.querydsl.core.Tuple;
 import com.querydsl.jpa.impl.JPAQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import ru.znamenka.api.page.sale.PaymentsApi;
 import ru.znamenka.jpa.repository.QueryFactory;
 
 import java.util.List;
 
-import static ru.znamenka.jpa.model.QPayment.payment;
 import static ru.znamenka.jpa.model.QProduct.product;
 import static ru.znamenka.jpa.model.QPurchase.purchase;
 
@@ -27,10 +25,10 @@ public class SaleService {
         JPAQuery<Tuple> query = factory.getJpaQuery();
         query
                 /// TODO: 11.08.2016 дописать селект
-                .select(1,1,1,1)
+
                 .from(purchase)
                 .leftJoin(purchase.product, product)
-                .where(purchase.client.id.eq(clientId).and(product.id.in(1,2,3)).and(product.expireDays.gt(0)));
+              ;
 
         return query.fetch();
     }
