0fc38c11f88a34e300b56946817f6bf23d0ec227
Evgeny.Utkin <evgeny.utkin@tns-global.ru>
2016-09-05 17:41:48 +0300

    fix

--- src/main/java/ru/znamenka/DemoApplication.java
+++ src/main/java/ru/znamenka/DemoApplication.java
@@ -2,6 +2,7 @@ package ru.znamenka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -16,4 +17,10 @@ public class DemoApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DemoApplication.class);
+	}
+
 }
