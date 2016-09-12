9e71441c80691b78768f79c8eb7053ee9536987f
Evgeny.Utkin <evgeny.utkin@tns-global.ru>
2016-09-12 11:15:35 +0300

    исправил 404 ошибку

--- src/main/resources/static/js/error_page.js
+++ src/main/resources/static/js/error_page.js
@@ -1,22 +1,24 @@
-var four = document.getElementById('wobble');
-
 function wobble() {
-  var dist = 60;
-  var id = setInterval(function() {
-    if(dist % 2 == 0) {
-      var deg = 180 + dist;
-    } else {
-      var deg = 180 - dist;
-    }
-    
-    four.style.transform = "rotate(" + deg + "deg)";
-    
-    if(dist > 15){
-      dist -= 11;
-    } else {
-      dist = -dist;
-    }
-  }, 1000);
+    var four = document.getElementById('wobble');
+    var dist = 60;
+    var id = setInterval(function () {
+        var deg;
+        if (dist % 2 == 0) {
+            deg = 180 + dist;
+        } else {
+            deg = 180 - dist;
+        }
+
+        four.style.transform = "rotate(" + deg + "deg)";
+
+        if (dist > 15) {
+            dist -= 11;
+        } else {
+            dist = -dist;
+        }
+    }, 1000);
 }
 
-window.addEventListener('load', function() {wobble()});
+window.addEventListener('load', function () {
+    wobble()
+});
