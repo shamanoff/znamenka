c5e6696c1ab8610d07269999a3914cd3bb016500
eutkin <utkinev2014@gmail.com>
2016-09-25 22:40:44 +0300

    исправил тайм

--- src/main/resources/static/js/end-of-day.js
+++ src/main/resources/static/js/end-of-day.js
@@ -7,7 +7,7 @@ function getTraining(date) {
         data: {
             "date": date.format('DD/MM/YYYY').toString()
         },
-        success: function(data) {
+        success: function (data) {
             $('#container').replaceWith(data);
             init(date.toDate())
         }
@@ -15,49 +15,15 @@ function getTraining(date) {
 }
 
 function init(date) {
-    $('#startTime')
-        .datetimepicker({
-            defaultDate: date,
-            format: 'DD/MM/YYYY'
-        });
-    $('#startTime').on('dp.change', function(e) {
+
+    var startTime = $('#startTime');
+
+    startTime.datetimepicker({
+        defaultDate: date,
+        format: 'DD/MM/YYYY'
+    });
+    startTime.on('dp.change', function (e) {
         getTraining(e.date);
     });
 
-    $('#contact_form').bootstrapValidator({
-        // To use feedback icons, ensure that you use Bootstrap v3.1.0 or later
-        feedbackIcons: {
-            valid: 'glyphicon glyphicon-ok',
-            invalid: 'glyphicon glyphicon-remove',
-            validating: 'glyphicon glyphicon-refresh'
-        },
-        fields: {
-            phone: {
-                validators: {
-
-                    notEmpty: {
-                        message: 'Введите статус тренировки'
-                    }
-                }
-            }
-        }
-    })
-        .on('success.form.bv', function (e) {
-            $('#success_message').slideDown({opacity: "show"}, "slow");// Do something ...
-            $('#contact_form').data('bootstrapValidator').resetForm();
-
-            // Prevent form submission
-            e.preventDefault();
-
-            // Get the form instance
-            var $form = $(e.target);
-
-            // Get the BootstrapValidator instance
-            var bv = $form.data('bootstrapValidator');
-
-            // Use Ajax to submit form data
-            $.post($form.attr('action'), $form.serialize(), function (result) {
-                console.log(result);
-            }, 'json');
-        });
 }
