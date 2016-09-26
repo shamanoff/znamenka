95a40fc0a5337ec0f8c41b716b3c1b5a591c6cf5
eutkin <utkinev2014@gmail.com>
2016-09-27 00:09:07 +0300

    исправил модель

--- src/main/java/ru/znamenka/jpa/model/ActionLog.java
+++ src/main/java/ru/znamenka/jpa/model/ActionLog.java
@@ -3,7 +3,9 @@ package ru.znamenka.jpa.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.time.LocalDateTime;
 
 import static java.time.LocalDateTime.now;
@@ -12,16 +14,23 @@ import static java.time.LocalDateTime.now;
 @Getter @Setter
 public class ActionLog implements BaseModel<Long>, LogEntity {
 
+    @Id
+    @Column(name = "id")
     private Long id;
 
+    @Column(name = "currenttime")
     private LocalDateTime currentTime;
 
+    @Column(name = "username")
     private String username;
 
+    @Column(name = "action")
     private String action;
 
+    @Column(name = "success")
     private Boolean success;
 
+    @Column(name = "message")
     private String message;
 
     public static ActionLog of(User user, String action, boolean success, String message) {
