a17bcb82938df62d6f1e308821c46cb69257e668
surikat666 <rina_seagull@mail.ru>
2016-12-04 15:54:16 +0300

    рефакторинг

--- reporting/src/main/java/ru/click/reporting/ClientPaymentsService.java
+++ reporting/src/main/java/ru/click/reporting/ClientPaymentsService.java
@@ -1,6 +1,43 @@
 package ru.click.reporting;
 
+import lombok.val;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Service;
+import ru.click.reporting.model.ClientPayments;
+import ru.click.reporting.query.QueryHolder;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.springframework.util.Assert.notNull;
+
+@Service
 public class ClientPaymentsService {
 
+    private final JdbcOperations operations;
 
+    private final QueryHolder clientPaymentsQueryHolder;
+
+    private static final RowMapper<ClientPayments> rowMapper = (rs, i) -> ClientPayments
+            .builder()
+            .clientName(rs.getString("client_name"))
+            .paymentDate(rs.getDate("payment_date"))
+            .productName(rs.getString("product_name"))
+            .sumPayments(rs.getLong("sum_payments"))
+            .build();
+
+    @Autowired
+    public ClientPaymentsService(JdbcOperations operations, QueryHolder clientPaymentsQueryHolder) {
+        notNull(operations, "Jdbc Operations must not be null");
+        notNull(clientPaymentsQueryHolder, "Query holder must not be null");
+        this.operations = operations;
+        this.clientPaymentsQueryHolder = clientPaymentsQueryHolder;
+    }
+
+    public List<ClientPayments> clientPayments(LocalDate from, LocalDate to, Long clientId, Long trainerId) {
+        val sql = clientPaymentsQueryHolder.getQuery();
+        return operations.query(sql, rowMapper, from, to);
+    }
 }
--- reporting/src/main/java/ru/click/reporting/query/ClientPaymentsQueryHolder.java
+++ reporting/src/main/java/ru/click/reporting/query/ClientPaymentsQueryHolder.java
@@ -1,37 +1,12 @@
 package ru.click.reporting.query;
 
-import lombok.extern.slf4j.Slf4j;
-import lombok.val;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcOperations;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.stereotype.Service;
-import ru.click.reporting.model.ClientPayments;
-
-import static org.springframework.util.Assert.notNull;
 import static ru.click.reporting.util.IOUtils.fileToString;
 
-@Slf4j
-@Service
 public class ClientPaymentsQueryHolder implements QueryHolder {
-
-    private final JdbcOperations operations;
-
-    private static final RowMapper<ClientPayments> rowMapper = (rs,i) -> ClientPayments
-            .builder()
-            .clientName(rs.getString("clientName"))
-            .build();
-
-    @Autowired
-    public ClientPaymentsQueryHolder(JdbcOperations operations) {
-        notNull(operations, "Jdbc Operations must not be null");
-        this.operations = operations;
-    }
 
     @Override
     public String getQuery() {
-        val sql = fileToString("/sql/client-payments.sql");
-        return null;
+        return fileToString("/sql/client-payments.sql");
     }
 
 }
